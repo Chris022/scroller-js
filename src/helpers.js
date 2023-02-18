af5a210e3bbecbf26af306c79256502c8ec06bdb
Christoph <christoph@debian>
2023-02-18 17:20:50 +0100

    :bug: remove negation from return

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ export function DataStorrage(element,slug){
         (name,default_val)=>u(element).data(slug+"-"+name) || default_val,
         (name)=>{
             if(!u(element).data(slug+"-"+name))throw new Error("Animation Object missing data attribute: " + slug + "-" + name)
-            return !u(element).data(slug+"-"+name)
+            return u(element).data(slug+"-"+name)
         }
     ]
 
