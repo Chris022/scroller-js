8866e4553d54d7f819301141ff44503a2d36a783
Christoph <christoph@debian>
2023-02-18 17:13:55 +0100

    add animation-function to object attribuest

--- src/scroll-controlled-animation.js
+++ src/scroll-controlled-animation.js
@@ -13,8 +13,12 @@ export class ScrollControlledAnimation{
         this.keyframe_to        = getDataError("keyframe-to");
         this.unit               = getDataError("unit");
         this.scroll_duration    = getDataError("scroll-length");
+        this.animation_function = getDataError("function")
 
         this.start_offset       = getDataDefault("start-offset", 0);
+
+        //process attributes
+        this.animation_function = animation_functions[this.animation_function]
 
         //set default values
         this.default_transform = element.style.transform
@@ -22,12 +26,12 @@ export class ScrollControlledAnimation{
     checkEvents(scroll){
 
         //verticaly stretch animation-function to start at "keyframe-from" and end at "keyframe-to"
-        let stretched_animation_function = (counter)=>(this.keyframe_from + animation_functions["linear"](counter) * (this.keyframe_to-this.keyframe_from))
+        let stretched_animation_function = (counter)=>(this.keyframe_from + this.animation_function(counter) * (this.keyframe_to-this.keyframe_from))
         
         let objectPosYAnchorMiddle = u(this.element).size().top + window.scrollY +  u(this.element).size().height/2
         
         let relative_scroll = scroll - objectPosYAnchorMiddle - this.start_offset
-        console.log(relative_scroll)
+
         if(relative_scroll < 0){
             this.element.style.transform = this.default_transform + " " + this.animated_transform+"("+this.keyframe_from+this.unit+")";
         }else if(relative_scroll < this.scroll_duration){
